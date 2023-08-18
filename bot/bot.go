6d7e890de4e3a6eae755558cb93a09d7bb6dcbf5
AnishP15 <39468239+AnishP15@users.noreply.github.com>
2023-08-18 16:06:22 -0700

    feat: Compiled binary

--- bot/bot.go
+++ bot/bot.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"cosmossdk.io/math"
 	"github.com/NibiruChain/nibiru/app"
 	"github.com/NibiruChain/nibiru/x/common"
 	"github.com/NibiruChain/nibiru/x/common/asset"
@@ -102,8 +103,9 @@ func Run(bot *Bot) error {
 	context := context.Background()
 
 	if err := bot.SyncState(); err != nil {
-		return err
+		return fmt.Errorf("Cannot SyncState(): %s", err)
 	}
+
 	// Querying info for Prices/Amms structs
 	err := bot.FetchNewPrices(context)
 	if err != nil {
@@ -312,7 +314,7 @@ func NewBot(args BotArgs) (*Bot, error) {
 
 	var keyName string = KEY_NAME
 
-	dontUseMnemonic := args.Mnemonic != "" || args.UseMnemonic == false
+	dontUseMnemonic := args.Mnemonic == "" || args.UseMnemonic == false
 
 	if !dontUseMnemonic {
 
@@ -338,10 +340,10 @@ func NewBot(args BotArgs) (*Bot, error) {
 		}
 
 		if !keyExists {
-
 			err = gonibi.AddSignerToKeyring(gosdk.Keyring, privKey, keyName)
 
 			if err != nil {
+
 				return nil, err
 
 			}
@@ -382,7 +384,7 @@ func (bot *Bot) OpenPosition(trader sdk.AccAddress, quoteToMove sdk.Int,
 		Sender:               trader.String(),
 		Pair:                 asset.Pair(pair),
 		Side:                 perpTypes.Direction(side),
-		QuoteAssetAmount:     quoteToMove.Abs(),
+		QuoteAssetAmount:     quoteToMove.Abs().Quo(math.NewInt(10)),
 		Leverage:             leverage,
 		BaseAssetAmountLimit: sdk.NewInt(0),
 	})
@@ -462,6 +464,7 @@ func (bot *Bot) GetKeyringRecord() (*keyring.Record, error) {
 }
 
 func (bot *Bot) GetAddress() (sdk.AccAddress, error) {
+
 	record, err := bot.GetKeyringRecord()
 
 	if err != nil {
--- cli/cli.go
+++ cli/cli.go
@@ -2,23 +2,27 @@ package cli
 
 import (
 	fbot "fbot/bot"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/urfave/cli"
 )
+
+// fix .env placement in tests
 
 func CliMain() {
 
 	app := cli.NewApp()
 	app.Name = "Funding Bot"
 
-	runner := fbot.Runner{
+	runner := &fbot.Runner{
 		Bot: &fbot.Bot{},
 		Server: &fbot.Server{
-			StartCh:  make(chan bool),
-			StopCh:   make(chan bool),
-			PauseCh:  make(chan bool),
+			StartCh:  false,
+			StopCh:   false,
+			PauseCh:  false,
 			IsPaused: false,
 		},
 	}
@@ -29,7 +33,7 @@ func CliMain() {
 		log.Fatal(err)
 	}
 
-	err = runner.SetConfig(*botConfig)
+	err = runner.SetConfig(*botConfig, "bot")
 
 	if err != nil {
 		panic(err)
@@ -37,26 +41,16 @@ func CliMain() {
 
 	app.Commands = []cli.Command{
 		{
-			// go run main.go start
 			Name: "start",
 			Action: func(c *cli.Context) error {
-
+				startTrade(runner)
 				return nil
 			},
 		},
 		{
-			// go run main.go pause
-			Name: "pause",
+			Name: "clear-db",
 			Action: func(c *cli.Context) error {
-
-				return nil
-			},
-		},
-		{
-			// go run main.go end
-			Name: "end",
-			Action: func(c *cli.Context) error {
-
+				runner.Bot.DB.ClearDB()
 				return nil
 			},
 		},
@@ -67,4 +61,78 @@ func CliMain() {
 	if err != nil {
 		panic(err)
 	}
+
+}
+
+func startTrade(runner *fbot.Runner) {
+	ch := make(chan string)
+
+	runner.Server.StartCh = true
+
+	for runner.Server.StartCh {
+
+		go func() {
+			var input string
+			time.Sleep(2 * time.Second)
+			fmt.Scanln(&input)
+			ch <- input
+		}()
+
+		select {
+		case cmd := <-ch:
+			if cmd == "pause" {
+				runner.Server.StartCh = false
+				runner.Server.PauseCh = true
+				pauseTrade(runner)
+			} else if cmd == "stop" {
+				runner.Server.StartCh = false
+				runner.Server.StopCh = true
+				stopTrade(runner)
+			}
+		case <-time.After(1 * time.Second):
+			runner.StartBot()
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
+
+func pauseTrade(runner *fbot.Runner) {
+	ch := make(chan string)
+
+	for runner.Server.PauseCh {
+
+		go func() {
+			var input string
+			time.Sleep(2 * time.Second)
+			fmt.Scanln(&input)
+			ch <- input
+		}()
+
+		select {
+		case cmd := <-ch:
+			if cmd == "start" {
+				runner.Server.StartCh = true
+				runner.Server.PauseCh = false
+				startTrade(runner)
+			} else if cmd == "stop" {
+				runner.Server.StopCh = true
+				runner.Server.PauseCh = false
+				stopTrade(runner)
+			}
+		case <-time.After(1 * time.Second):
+			fmt.Println("Pausing Bot")
+			time.Sleep(2 * time.Second)
+
+		}
+	}
+}
+
+func stopTrade(runner *fbot.Runner) {
+
+	fmt.Println("Exiting positions")
+	err := runner.EndBot()
+	if err != nil {
+		log.Fatalf("Failed to exit positions")
+	}
+
 }
